3a3ef53c62dd0241513709520cfac59443045a6d
moges <eyobaklilu175@gmail.com>
2026-03-29 21:19:33 +0300

    solve the 6th problem

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(stri,num) {
+    let str="";
+    if(num<1){
+        return 'ERROR';
+    }
+    else{
+        for(let i=0;i<num;i++){
+            str+=stri;
+        }
+    }
+    return str;
 
 };
 
