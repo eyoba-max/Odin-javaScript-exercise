35542af5308dcf90d873a1422cf71aa42c338998
moges <eyobaklilu175@gmail.com>
2026-03-30 10:11:01 +0300

    solve 8th problem

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value) {
+    const index = arr.indexOf(value);
+    if (index !== -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
