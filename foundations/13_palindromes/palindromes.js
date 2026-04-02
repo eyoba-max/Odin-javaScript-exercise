8536c2798bbc14e5070b67da1a28c54f3c074e76
moges <eyobaklilu175@gmail.com>
2026-04-02 09:42:02 +0300

    solve the 13th problem

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphaNumeric='abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleaned=word
+    .toLowerCase()
+    .split('')
+    .filter((character)=>alphaNumeric.includes(character))
+    .join('');
 
+    const reversed=cleaned.split('').reverse().join('');
+    return reversed===cleaned;
 };
 
 // Do not edit below this line
